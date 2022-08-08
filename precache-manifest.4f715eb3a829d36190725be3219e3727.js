12661925cb5f6b64ce1691a4a74a3fe766236d9a
sindhi2210 <abhaynathani64@gmail.com>
2022-08-08 11:33:31 +0530

    Initial Commit

---
+++ precache-manifest.4f715eb3a829d36190725be3219e3727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ecf55c6d792c3d52e7a",
+    "url": "/bert_tfjs_qna_app/static/css/main.3758d443.chunk.css"
+  },
+  {
+    "revision": "1ecf55c6d792c3d52e7a",
+    "url": "/bert_tfjs_qna_app/static/js/main.1ecf55c6.chunk.js"
+  },
+  {
+    "revision": "275e4a3dcd41dfcc16eb",
+    "url": "/bert_tfjs_qna_app/static/js/1.275e4a3d.chunk.js"
+  },
+  {
+    "revision": "9f51260d613a6cc05f4d",
+    "url": "/bert_tfjs_qna_app/static/js/2.9f51260d.chunk.js"
+  },
+  {
+    "revision": "e3eacebd50ccbe168ff3",
+    "url": "/bert_tfjs_qna_app/static/js/runtime~main.e3eacebd.js"
+  },
+  {
+    "revision": "22385325bc1c270b54c8b1c0cee86550",
+    "url": "/bert_tfjs_qna_app/index.html"
+  }
+];
